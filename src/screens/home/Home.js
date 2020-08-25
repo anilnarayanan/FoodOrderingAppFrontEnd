2949c03ac83c0f5d04b7409c0aef5ef5c8b4af55
sowmya <jsowmya@outlook.com>
2020-08-25 22:03:48 +0530

    Adding methods for Cards

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -18,13 +18,88 @@ class Home extends Component {
         }
     }
 
-    render() {
+    componentWillMount(){
+            // Intialize the cart
+            sessionStorage.removeItem('customer-cart');
+            // get restaurants from api
+            let dataRestaurants = null;
+            let xhrRestaurants = new XMLHttpRequest();
+            xhrRestaurants.addEventListener('readystatechange', function () {
+                if (this.readyState === 4) {
+                    this.setState({
+                        restaurants: JSON.parse(this.responseText).restaurants
+                    })
+                }
+            })
+            xhrRestaurants.open('GET', `${this.props.baseUrl}restaurant`);
+            xhrRestaurants.send(dataRestaurants);
+
+            this.updateCardsGridListCols();
+        }
+
+        componentDidMount(){
+            window.addEventListener('resize', this.updateCardsGridListCols);
+        }
+
+        componentWillUnmount(){
+            window.removeEventListener('resize', this.updateCardsGridListCols);
+        }
+
+        updateCardsGridListCols = () => {
+            if (window.innerWidth >= 1300) {
+                this.setState({ cards: 4 });
+                return;
+            }
+
+            if (window.innerWidth >= 1000) {
+                this.setState({ cards: 3 });
+                return;
+            }
+
+            this.setState({ cards: 2 });
+        }
+        restaurantCardTileOnClickHandler = (restaurantId) => {
+            this.props.history.push('/restaurant/' + restaurantId);
+        }
+
+        searchHandler = (event) => {
+            let that = this;
+            let dataRestaurants = null;
+            let xhrRestaurants = new XMLHttpRequest();
+            xhrRestaurants.addEventListener('readystatechange', function () {
+                if (this.readyState === 4) {
+                    if (!JSON.parse(this.responseText).restaurants) {
+                        that.setState({
+                            restaurants: null,
+                        })
+                    } else {
+                        that.setState({
+                            restaurants: JSON.parse(this.responseText).restaurants,
+                        })
+                    }
+                }
+            })
+            if (event.target.value === ''){
+                xhrRestaurants.open('GET', `${this.props.baseUrl}restaurant`);
+            } else {
+                xhrRestaurants.open('GET', `${this.props.baseUrl}restaurant/name/${event.target.value}`);
+            }
+            xhrRestaurants.send(dataRestaurants);
+        }
+
+
+
+
+    render(){
+        const { classes } = this.props;
         return (
             <div>
-                <Header/>
+                <Header
+                    showSearch="true"
+                    searchHandler={this.searchHandler}
+                />
             </div>
-        )
+        );
     }
 }
-
 export default Home;
