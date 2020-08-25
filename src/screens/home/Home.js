f1f6552b8eae3f3a75e187c38e956d0424192a10
sowmya <jsowmya@outlook.com>
2020-08-25 17:20:54 +0530

    Home Page Commit Check

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import Header from '../../common/header/Header';
 import './Home.css';
-
+import 'font-awesome/css/font-awesome.min.css';
+import GridList from '@material-ui/core/GridList';
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
+import GridListTile from '@material-ui/core/GridListTile';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 class Home extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            restaurants: [],
+            cards: 0
+        }
     }
 
     render() {
