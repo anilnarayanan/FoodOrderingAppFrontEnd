b335182da8b817c327a7ce9f4ec9f548989459b7
Anil Narayanan <narayanan.anil@gmail.com>
2020-08-29 22:04:59 +0530

    Updated functionality in details.

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,54 +1,29 @@
 import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
-import { red, grey } from "@material-ui/core/colors";
-import ShareIcon from "@material-ui/icons/Share";
-import { makeStyles } from "@material-ui/core/styles";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Avatar from "@material-ui/core/Avatar";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import Collapse from "@material-ui/core/Collapse";
+import Badge from "@material-ui/core/Badge";
 import "./Details.css";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import AddIcon from '@material-ui/icons/Add';
+import { FaStar } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa";
+import { FaCircle } from "react-icons/fa";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-    backgroundColor: grey,
-  },
-  media: {
-    height: 215,
-    paddingTop: "56.25%", // 16:9
-    width: 151,
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: "rotate(180deg)",
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-}));
-
-class Details extends Component {
+export default class Details extends Component {
   constructor() {
     super();
     this.state = {
       restaurantList: [],
+      categoryList: [],
+      cartList: [],
+      totAmt: 0,
     };
   }
 
@@ -61,25 +36,26 @@ class Details extends Component {
         console.log(this.responseText);
 
         let httpResponse = JSON.parse(xhr.responseText);
+        let category;
         let categories = [];
         httpResponse.categories.forEach((category) => {
           categories.push(category.category_name);
         });
-
-        let restaurantList = {
+        category = httpResponse.categories;
+        let restaurantDict = {
           id: httpResponse.id,
-          photoURL: httpResponse.photo_URL,
+          photoUrl: httpResponse.photo_URL,
           name: httpResponse.restaurant_name,
           avgCost: httpResponse.average_price,
           rating: httpResponse.customer_rating,
           noOfCustomerRated: httpResponse.number_customers_rated,
           locality: httpResponse.address.locality,
-          categoriesName: categories.toString(),
+          categories: categories.toString(),
         };
-
         that.setState({
           ...that.state,
-          restaurantList: restaurantList,
+          restaurantList: restaurantDict,
+          categoryList: category,
         });
       }
     });
@@ -90,101 +66,292 @@ class Details extends Component {
     xhr.send(data);
   }
 
-  render() {
-    // const classes = useStyles();
-    // const [expanded, setExpanded] = React.useState(false);
+  itemAddOnClickHandler = (item) => {
+    let cartItems = this.state.cartItems;
+    let itemInCart = false;
+    cartItems.forEach((cartItem) => {
+      if (cartItem.id === item.id) {
+        itemInCart = true;
+        cartItem.quantity++;
+        cartItem.totalAmount = cartItem.price * cartItem.quantity;
+      }
+    });
+    if (!itemInCart) {
+      let cartItem = {
+        id: item.id,
+        name: item.item_name,
+        price: item.price,
+        totalAmount: item.price,
+        quantity: 1,
+        itemType: item.item_type,
+      };
+      cartItems.push(cartItem);
+    }
+    let totalAmount = 0;
+    cartItems.forEach((cartItem) => {
+      totalAmount = totalAmount + cartItem.totalAmount;
+    });
 
-    // const handleExpandClick = () => {
-    //   setExpanded(!expanded);
-    // };
+    this.setState({
+      ...this.state,
+      cartItems: cartItems,
+      snackBarOpen: true,
+      snackBarMessage: "Item added to cart!",
+      totalAmount: totalAmount,
+    });
+  };
+
+  render() {
     const classes = this.props;
     return (
       <div>
-      <div>
-        {/* <Card>
-          <CardContent className="root"> */}
-        <div>
-          <img
-            src={this.state.restaurantList.photoURL}
-            alt={this.state.restaurantList.photoURL}
-            height="215px"
-            width="275px"
-          />
-        </div>
-        <div className="restaurant-details">
-          <div className="restaurant-name">
-            <Typography
-              variant="h5"
-              component="h5"
-              className={classes.restaurantName}
-            >
-              {this.state.restaurantList.name}
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              component="p"
-              className={classes.restaurantLocation}
-            >
-              {this.state.restaurantList.locality}
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              component="p"
-              className={classes.restaurantCategory}
-            >
-              {this.state.restaurantList.categoriesName}
-            </Typography>
+        <div className="rest_list">
+          <div>
+            <img
+              src={this.state.restaurantList.photoUrl}
+              alt="Restaurant"
+              height="215px"
+              width="275px"
+            />
           </div>
-          <div className="restaurant-rating-cost-container">
-            <div className="restaurant-rating-container">
-              <div className="restaurant-rating">
-                {/* <FontAwesomeIcon icon="star" size="sm" color="black" /> */}
-                <Typography variant="subtitle1" component="p">
-                  {this.state.restaurantList.rating}
-                </Typography>
-              </div>
+          <div className="rest_details">
+            <div className="restaurant-name">
               <Typography
-                variant="caption"
-                component="p"
-                className={classes.textRatingCost}
+                variant="h5"
+                component="h5"
+                className={classes.restaurantName}
               >
-                AVERAGE RATING BY{" "}
-                {
-                  <span className="restaurant-NoOfCustomerRated">
-                    {this.state.restaurantList.noOfCustomerRated}
-                  </span>
-                }{" "}
-                CUSTOMERS
+                {this.state.restaurantList.name}
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                component="p"
+                className={classes.restaurantLocation}
+              >
+                {this.state.restaurantList.locality}
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                component="p"
+                className={classes.restaurantCategory}
+              >
+                {this.state.restaurantList.categoriesName}
               </Typography>
             </div>
-            <div className="restaurant-avg-cost-container">
-              <div className="restaurant-avg-cost">
-                <i className="fa fa-inr" aria-hidden="true"></i>
+            <div className="rest_rating_cost_parent">
+              <div className="restaurant-rating-container">
+                <div className="rest_rating">
+                  <FaStar />
+                  <Typography variant="subtitle1" component="p">
+                    {this.state.restaurantList.rating}
+                  </Typography>
+                </div>
                 <Typography
-                  variant="subtitle1"
+                  variant="caption"
                   component="p"
-                  className={classes.avgCost}
+                  className={classes.textRatingCost}
                 >
-                  {this.state.restaurantList.avgCost}
+                  AVERAGE RATING BY{" "}
+                  {
+                    <span className="rest_cust_rated">
+                      {this.state.restaurantList.noOfCustomerRated}
+                    </span>
+                  }{" "}
+                  CUSTOMERS
                 </Typography>
               </div>
-              <Typography
-                variant="caption"
-                component="p"
-                className={classes.textRatingCost}
-              >
-                AVERAGE COST FOR TWO PEOPLE
-              </Typography>
+              <div className="rest_avgcost_parent">
+                <div className="rest_avgcost">
+                  <i className="fa fa-inr" aria-hidden="true"></i>
+                  <Typography
+                    variant="subtitle1"
+                    component="p"
+                    className={classes.avgCost}
+                  >
+                    {this.state.restaurantList.avgCost}
+                  </Typography>
+                </div>
+                <Typography
+                  variant="caption"
+                  component="p"
+                  className={classes.textRatingCost}
+                >
+                  AVERAGE COST FOR TWO PEOPLE
+                </Typography>
+              </div>
             </div>
           </div>
         </div>
-        {/* </CardContent>
-        </Card> */}
-      </div>
-      
+        <div className="menu_details_cart_parent">
+          <div className="menu_details">
+            {this.state.categoryList.map((category) => (
+              <div key={category.id}>
+                <Typography
+                  variant="overline"
+                  component="p"
+                  className={classes.categoryName}
+                >
+                  {category.category_name}
+                </Typography>
+                <Divider />
+                {category.item_list.map((item) => (
+                  <div className="menu_item_parent" key={item.id}>
+                    <FaCircle
+                      color={
+                        item.item_type === "NON_VEG" ? "#BE4A47" : "#5A9A5B"
+                      }
+                    />
+                    &nbsp;&nbsp;
+                    <Typography
+                      variant="subtitle1"
+                      component="p"
+                      className={classes.menuItemName}
+                    >
+                      {item.item_name[0].toUpperCase() +
+                        item.item_name.slice(1)}
+                    </Typography>
+                    <div className="item_price">
+                      <i className="fa fa-inr" aria-hidden="true"></i>
+                      <Typography
+                        variant="subtitle1"
+                        component="p"
+                        className={classes.itemPrice}
+                      >
+                        {item.price.toFixed(2)}
+                      </Typography>
+                    </div>
+                    <IconButton
+                      className={classes.addButton}
+                      aria-label="add"
+                      onClick={() => this.itemAddOnClickHandler(item)}
+                    >
+                      <AddIcon />
+                    </IconButton>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="my_cart">
+          <Card className={classes.myCart}>
+            <CardHeader
+              avatar={
+                <Avatar
+                  aria-label="shopping-cart"
+                  className={classes.shoppingCart}
+                >
+                  <Badge
+                    badgeContent={this.state.cartList.length}
+                    color="primary"
+                    showZero={true}
+                    invisible={this.state.badgeVisible}
+                    className={classes.badge}
+                  >
+                    <ShoppingCartIcon />
+                  </Badge>
+                </Avatar>
+              }
+              title="My Cart"
+              titleTypographyProps={{
+                variant: "h6",
+              }}
+              className={classes.cartHeader}
+            />
+            <CardContent className={classes.cardContent}>
+              {this.state.cartList.map((cartItem) => (
+                <div className="cart_menu_item_name" key={cartItem.id}>
+                  <i
+                    className="fa fa-stop-circle-o"
+                    aria-hidden="true"
+                    style={{
+                      color:
+                        cartItem.itemType === "NON_VEG" ? "#BE4A47" : "#5A9A5B",
+                    }}
+                  ></i>
+                  <Typography
+                    variant="subtitle1"
+                    component="p"
+                    className={classes.menuItemName}
+                    id="cart-menu-item-name"
+                  >
+                    {cartItem.name[0].toUpperCase() + cartItem.name.slice(1)}
+                  </Typography>
+                  <div className="quantity_parent">
+                    <IconButton
+                      className={classes.cartItemButton}
+                      id="minus_button"
+                      aria-label="remove"
+                      onClick={() => this.minusButtonClickHandler(cartItem)}
+                    >
+                      <FaMinus />
+                    </IconButton>
+                    <Typography
+                      variant="subtitle1"
+                      component="p"
+                      className={classes.itemQuantity}
+                    >
+                      {cartItem.quantity}
+                    </Typography>
+                    <IconButton
+                      className={classes.cartItemButton}
+                      aria-label="add"
+                      onClick={() => this.cartAddButtonClickHandler(cartItem)}
+                    >
+                      <FaPlus />
+                    </IconButton>
+                  </div>
+                  <div className="item-price">
+                    <i
+                      className="fa fa-inr"
+                      aria-hidden="true"
+                      style={{ color: "grey" }}
+                    ></i>
+                    <Typography
+                      variant="subtitle1"
+                      component="p"
+                      className={classes.itemPrice}
+                      id="cart_item_price"
+                    >
+                      {cartItem.totAmt.toFixed(2)}
+                    </Typography>
+                  </div>
+                </div>
+              ))}
+              <div className="total_amt_parent">
+                <Typography
+                  variant="subtitle2"
+                  component="p"
+                  className={classes.totalAmount}
+                >
+                  TOTAL AMOUNT
+                </Typography>
+                <div className="total_price">
+                  <i className="fa fa-inr" aria-hidden="true"></i>
+                  <Typography
+                    variant="subtitle1"
+                    component="p"
+                    className={classes.itemPrice}
+                    id="cart_tot_price"
+                  >
+                    {this.state.totAmt.toFixed(2)}
+                  </Typography>
+                </div>
+              </div>
+
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth={true}
+                className={classes.checkOutButton}
+                onClick={this.checkOutButtonClickHandler}
+              >
+                CHECKOUT
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     );
   }
 }
-
-export default Details;
