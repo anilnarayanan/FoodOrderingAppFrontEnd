ba408ace7ea56267149bfd4d044b46c76bc29495
sowmya <jsowmya@outlook.com>
2020-08-30 14:27:57 +0530

    Committing the rendering

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -31,18 +31,10 @@ class Home extends Component {
                     })
                 }
             })
-            xhrRestaurants.open('GET', `${this.props.baseUrl}restaurant`);
+            xhrRestaurants.open('GET', this.props.baseUrl + 'restaurant');
             xhrRestaurants.send(dataRestaurants);
 
             this.updateCardsGridListCols();
-        }
-
-        componentDidMount(){
-            window.addEventListener('resize', this.updateCardsGridListCols);
-        }
-
-        componentWillUnmount(){
-            window.removeEventListener('resize', this.updateCardsGridListCols);
         }
 
         updateCardsGridListCols = () => {
@@ -58,6 +50,14 @@ class Home extends Component {
 
             this.setState({ cards: 2 });
         }
+        componentDidMount() {
+            window.addEventListener('resize', this.updateCardsGridListCols);
+        }
+
+        componentWillUnmount() {
+            window.removeEventListener('resize', this.updateCardsGridListCols);
+        }
+
         restaurantCardTileOnClickHandler = (restaurantId) => {
             this.props.history.push('/restaurant/' + restaurantId);
         }
@@ -80,9 +80,9 @@ class Home extends Component {
                 }
             })
             if (event.target.value === ''){
-                xhrRestaurants.open('GET', `${this.props.baseUrl}restaurant`);
+                xhrRestaurants.open('GET', this.props.baseUrl+ 'restaurant');
             } else {
-                xhrRestaurants.open('GET', `${this.props.baseUrl}restaurant/name/${event.target.value}`);
+                xhrRestaurants.open('GET', this.props.baseUrl+ 'restaurant/name/'+ event.target.value);
             }
             xhrRestaurants.send(dataRestaurants);
         }
@@ -91,13 +91,56 @@ class Home extends Component {
 
 
     render(){
-        const { classes } = this.props;
         return (
             <div>
                 <Header
-                    showSearch="true"
+                    showSearch={true}
                     searchHandler={this.searchHandler}
                 />
+                {this.state.restaurants === null ?
+                    <Typography className='null-Restaurant' variant='h6'>
+                        No restaurant with the given name.
+                    </Typography>
+                    :
+                    <GridList
+                        className='restaurant-GridList'
+                        cols={this.state.cards}
+                        cellHeight='auto'
+                    >
+                        {this.state.restaurants.map(restaurant => (
+                            <GridListTile
+                                onClick={() => this.restaurantCardTileOnClickHandler(restaurant.id)}
+                                key={'restaurant' + restaurant.id}
+                            >
+                                <Card className='restaurent-Card' style={{ textDecoration: 'none' }}>
+                                    <CardMedia
+                                        className='restaurent-CardMedia'
+                                        image={restaurant.photo_URL}
+                                        title={restaurant.restaurant_name}
+                                    />
+                                    <CardContent className='cardContent'>
+                                        <Typography className='restaurantName' gutterBottom variant='h5' component='h2'>
+                                            {restaurant.restaurant_name}
+                                        </Typography>
+                                        <Typography variant='subtitle1' className='categories'>
+                                            {restaurant.categories}
+                                        </Typography>
+                                        <div className='restaurant-RateSlab'>
+                                            <div className='restaurant-Rate'>
+                                                <i className="fa fa-star" aria-hidden="true"> </i>
+                                                {restaurant.customer_rating}({restaurant.number_customers_rated})
+                                            </div>
+                                            <div>
+                                                <i className="fa fa-inr" aria-hidden="true">
+                                                    <span>{restaurant.average_price} for two</span> </i>
+                                            </div>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </GridListTile>
+                        ))}
+                    </GridList>
+                }
             </div>
         );
     }
